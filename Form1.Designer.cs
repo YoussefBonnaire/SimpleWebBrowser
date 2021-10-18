d0fda6d7afad52a1b07f1d94167b4f1c778e69ad
Youssef Bonnaire <yb2010@hw.ac.uk>
2021-10-18 18:22:50 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace CW1_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Goback = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.Button();
+            this.Refresh = new System.Windows.Forms.Button();
+            this.Progress = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // Goback
+            // 
+            this.Goback.Location = new System.Drawing.Point(12, 16);
+            this.Goback.Name = "Goback";
+            this.Goback.Size = new System.Drawing.Size(75, 23);
+            this.Goback.TabIndex = 0;
+            this.Goback.Text = "<";
+            this.Goback.UseVisualStyleBackColor = true;
+            this.Goback.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(367, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(547, 16);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(55, 23);
+            this.Search.TabIndex = 3;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            // 
+            // Refresh
+            // 
+            this.Refresh.Location = new System.Drawing.Point(608, 16);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(72, 23);
+            this.Refresh.TabIndex = 4;
+            this.Refresh.Text = "Refresh";
+            this.Refresh.UseVisualStyleBackColor = true;
+            // 
+            // Progress
+            // 
+            this.Progress.AutoSize = true;
+            this.Progress.Location = new System.Drawing.Point(0, 429);
+            this.Progress.Name = "Progress";
+            this.Progress.Size = new System.Drawing.Size(51, 13);
+            this.Progress.TabIndex = 5;
+            this.Progress.Text = "Progress:";
+            this.Progress.Click += new System.EventHandler(this.Progress_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(68, 429);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(192, 13);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Progress);
+            this.Controls.Add(this.Refresh);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Goback);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Goback;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button Refresh;
+        private System.Windows.Forms.Label Progress;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW1_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Browser mybrowser = Browser.Search("https://docs.microsoft.com/");
+        }
+
+        
+    }
+    public class Browser
+    {
+        public System.Collections.Generic.List<Uri>.Enumerator History { get; }
+        public System.Collections.Generic.List<Uri>.Enumerator Favourites { get; }
+
+        public void Search(string source)
+        {
+
+            // Create a request for the URL.
+            WebRequest request = WebRequest.Create(
+              "source");
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            // Get the stream containing content returned by the server.
+            // The using block ensures the stream is automatically closed.
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.
+                Console.WriteLine(responseFromServer);
+            }
+
+            // Close the response.
+            response.Close();
+        }
+
+        public void Goback()
+        {
+
+        }
+        public void GoForward()
+        {
+
+        }
+
+        public void refresh()
+        {
+
+        }
+
+    }
+}
